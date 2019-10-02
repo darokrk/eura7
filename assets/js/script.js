2ada4e5846f107f644a1e494ff16652866d5b91b
Dariusz <dariusz.rosiek@eura7.pl>
2019-10-02 14:07:43 +0200

    Proto version with mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-  $(".first-button").on("click", function() {
-    $(".animated-icon1").toggleClass("open");
-  });
-  $(".second-button").on("click", function() {
-    $(".animated-icon2").toggleClass("open");
-  });
-  $(".third-button").on("click", function() {
-    $(".animated-icon3").toggleClass("open");
-  });
+$(document).ready(function () {
+    $(".first-button").on("click", function () {
+        $(".animated-icon1").toggleClass("open");
+    });
+    $(".second-button").on("click", function () {
+        $(".animated-icon2").toggleClass("open");
+    });
+    $(".third-button").on("click", function () {
+        $(".animated-icon3").toggleClass("open");
+    });
+
+    $(".navbar-toggler").click(function () {
+        $('.language-nav').toggleClass("d-none");
+    });
 });
